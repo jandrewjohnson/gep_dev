af5793c51e974d67b9157edf6013f31b9fe25491
Justin Johnson <jajohns@umn.edu>
2025-08-05 13:56:09 -0500

    Started integration for gep_dev repo calling this code. Stuck on the root project directory of a QMD called from a different repo. START HERE and consider using get_projectflow_module_root() or something similar to define a different type of "headless?" ProjectFlow object.

--- gep/gep_initialize_project.py
+++ gep/gep_initialize_project.py
@@ -1,13 +1,18 @@
 import pandas as pd
 import hazelbean as hb
 
-from global_invest.commercial_agriculture import commercial_agriculture_tasks
+from global_invest.crop_provision import crop_provision_initialization
+from global_invest.livestock_provision import livestock_provision_initialization
+from global_invest.renewable_energy_provision import renewable_energy_provision_initialization
 
 def initialize_paths(p):
-    p.ee_r264_df = pd.read_csv(p.countries_csv_path)
+    p.df_countries = pd.read_csv(p.df_countries_csv_path)  
+    p.gdf_countries = p.gdf_countries_vector_path 
+    p.gdf_countries_simplified = p.gdf_countries_vector_simplified_path 
     
+def build_gep_task_tree(p):
     
-def build_gep_commercial_agriculture_task_tree(p):
-    
-    p = commercial_agriculture_tasks.build_gep_task_tree(p)
+    p = crop_provision_initialization.build_gep_service_task_tree(p)
+    p = livestock_provision_initialization.build_gep_service_task_tree(p)
+    p = renewable_energy_provision_initialization.build_gep_service_task_tree(p)
     
--- gep/run_gep.py
+++ gep/run_gep.py
@@ -1,13 +1,32 @@
-global_gdp = 110.55e12  # Global GEP in USD
-lambda_value = 0.5  # Average lambda value for pollination services
+import os
+import pandas as pd
+import hazelbean as hb
 
-# Print a dot every tenth of a a second for 1 second
-print("Calculating GEP. Please wait, this may take a couple seconds.")
-import time
-for i in range(60):
-    print('.', end='', flush=True)
-    time.sleep(0.033333333)
+import gep_initialize_project
+
+if __name__ == '__main__':
     
-gep = global_gdp * lambda_value # Calculate Global Ecosystem Production (GEP)
+    """Simplified run file that assumes the user has already run the project and just wants to rerender the results."""
+    
+    # ProjectFlow object
+    p = hb.ProjectFlow() # Create a ProjectFlow Object to organize directories and enable parallel processing.
+    p.project_name = 'gep'  # Determines the folder created to store intermediate and final results.
+    p.project_dir = os.path.join(os.path.expanduser('~'), 'Files', 'gep', 'Projects', p.project_name) # Put it in the right location relative to the user's home directory.
+    p.base_data_dir = "G:/Shared drives/NatCapTEEMs/Files/base_data" # Set where data outside the project will be stored. CAUTION: For GEP we are using the shared Google Drive, but best practice is to use a local directory that you can control (also it's faster)
+    p.set_project_dir(p.project_dir) # Set the project directory in the ProjectFlow object. Also defines p.input_dir, p.intermediate_dir, and p.output_dir based on the project_dir.
+    
+    # Task tree
+    gep_initialize_project.build_gep_task_tree(p) # Defines the actual logic of the model. Navigate into here to see what the model does.
 
-print(f"\nYour GEP this year is {gep} (based on average lambda of {lambda_value}). Well done! Would you like me to optimize?")
+    # Project level attributes
+    p.df_countries_csv_path = p.get_path('cartographic', 'ee', 'ee_r264_correspondence.csv') # ProjectFlow downloads all files automatically via the p.get_path() function. 
+    p.gdf_countries_vector_path = p.get_path('cartographic', 'ee', 'ee_r264_correspondence.gpkg') 
+    p.gdf_countries_vector_simplified_path = p.get_path('cartographic', 'ee', 'ee_r264_simplified30sec.gpkg') 
+    p.results = {}  # All results will be stored here by each child task.
+    gep_initialize_project.initialize_paths(p)
+
+    # Run the model
+    hb.log('Created ProjectFlow object at ' + p.project_dir + '\n    from script ' + p.calling_script + '\n    with base_data set at ' + p.base_data_dir)    
+    p.execute()
+    
+    result = 'Done!'
